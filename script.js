7ce314b8b8db4a8144fefe8b9eae243fc5671dc3
Ricardo Silva <ricardo.rechena@hotmail.com>
2025-02-11 00:06:41 +0000

    Convite

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("noBtn").addEventListener("mouseover", function() {
+    let x = Math.random() * window.innerWidth - 100;
+    let y = Math.random() * window.innerHeight - 50;
+    this.style.position = "absolute";
+    this.style.left = `${x}px`;
+    this.style.top = `${y}px`;
+});
